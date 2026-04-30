b2b32565def3760588db30fe1f946fe4215d45b5
Bytebuilders <bytebuilders2026@gmail.com>
2026-04-30 10:47:25 +0530

    Update main.jsx

--- School-App-Frontend/src/main.jsx
+++ School-App-Frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from "react-router-dom";
 import axios from "axios";
 import './index.css'
 import App from './App.jsx'
@@ -31,8 +32,10 @@ axios.interceptors.response.use(
   }
 );
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
