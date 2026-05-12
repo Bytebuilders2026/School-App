c3f2951b6a393b299e2ed6ef7a903b4b008e0b8b
Kumarrohit93 <rk4840369@gmail.com>
2026-05-12 10:40:41 +0530

    Some Minor Changes

--- School-App-Frontend/src/api.ts
+++ School-App-Frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://school-app-4p2u.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
